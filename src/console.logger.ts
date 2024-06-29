25dfcef5f2a9423e0749ffa47ae15f3e423fed00
Robert Dean <robert@flexbase.app>
2024-06-29 13:13:48 -0400

    adding custom console constructor to console logger

--- src/console.logger.ts
+++ src/console.logger.ts
@@ -4,8 +4,10 @@ import { Logger } from './logger.js';
  * Represents a type that implements a console based logger
  */
 export class ConsoleLogger implements Logger {
+  constructor(private readonly _console: Console = console) {}
+
   private log(level: 'error' | 'warn' | 'info' | 'debug' | 'trace', message: unknown, optionalParams: unknown[]) {
-    optionalParams.length > 0 ? console[level](message, optionalParams) : console[level](message);
+    optionalParams.length > 0 ? this._console[level](message, optionalParams) : this._console[level](message);
   }
 
   error(message: unknown, ...optionalParams: unknown[]): void {
--- tests/ConsoleLogger.test.ts
+++ tests/ConsoleLogger.test.ts
@@ -1,5 +1,7 @@
 import { test, expect, vi } from 'vitest';
 import { ConsoleLogger } from '../src/index';
+import { Console } from 'console';
+import { stdout } from 'process';
 
 test('ConsoleLogger error', () => {
   const logger = new ConsoleLogger();
@@ -49,4 +51,20 @@ test('ConsoleLogger trace', () => {
   logger.trace('test');
 
   expect(trace).toBeCalledWith('test');
+});
+
+test('ConsoleLogger custom console provided', () => {
+  const _console = new Console({
+    stdout,
+  });
+
+  const logger = new ConsoleLogger(_console);
+
+  const info = vi.spyOn(_console, 'info');
+  const infoOriginal = vi.spyOn(console, 'info');
+
+  logger.info('test');
+
+  expect(info).toBeCalledWith('test');
+  expect(infoOriginal).not.toBeCalled();
 });
